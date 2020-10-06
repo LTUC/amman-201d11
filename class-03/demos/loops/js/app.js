800922f9247fbb1d8eb6741a8f37ffb1533f39c3
RazanAl-Quran <r.alquran@ltuc.com>
2020-10-06 11:41:34 +0300

    class03 done

---
+++ class-03/demos/loops/js/app.js
@@ -0,0 +1,87 @@
+// Loops
+// For & while
+
+// For loop
+// 0 to 9
+
+// for (var i=0 ; i<10 ; i++) {
+//     console.log(i);
+// }
+
+// var foodILike = ['makmoora','kabsa','burger','maqloba']; // 4 elements
+// // console.log(foodILike[0]);
+// // console.log(foodILike[1]);
+// // console.log(foodILike[2]);
+// // console.log(foodILike[3]);
+
+// console.log('length= ', foodILike.length);
+
+// for(var i=0; i<foodILike.length; i++) { //i<4 
+//     console.log(i); // 0 1 2 3
+//     console.log(foodILike[i]);
+// }
+
+
+// While loop
+// for (var i=0 ; i<10 ; i++) {
+//     console.log(i);
+// }
+
+// var i=0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+
+
+// var uName = prompt('what is ur name?');
+// while(uName !== 'razan') {
+//     uName = prompt('what is ur name plz ?')
+// }
+// console.log(`hello ${uName}`)
+
+
+
+// do-while
+
+// var index = 0;
+// do {
+//     console.log(index);
+//     index++;
+// }while(index<10)
+
+
+
+// var index2 = 1;
+// do {
+//     console.log(index2);
+//     index2++;
+// }while(index2<1)
+
+//output: 1
+
+
+
+// var index2 = 1;
+// while(index2<1) {
+//     console.log(index2);
+//     index2++;
+// }
+// output: nothing
+
+// break
+
+// for (var z=0; z<10 ; z++) {
+//     console.log(z);
+//     if(z===4) {
+//         break;
+//     }
+// }
+
+
+var c;
+if(c) {
+    console.log('truthy value ',c);
+} else {
+    console.log('falsy value ',c);
+}
