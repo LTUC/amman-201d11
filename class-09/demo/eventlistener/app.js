5184e8184aeff506d695061b27fbdc6267e3ad4c
RazanAl-Quran <r.alquran@ltuc.com>
2020-10-14 11:26:06 +0300

    class09 done

---
+++ class-09/demo/eventlistener/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var clickMeBtn = document.getElementById('clickMe');
+console.log(clickMeBtn);
+
+// clickMeBtn.addEventListener('click',sayHi);
+
+// function sayHi() {
+//     alert('you clicked me');
+// }
+
+
+clickMeBtn.addEventListener('click', function (){
+    alert('you clicked me 2');
+})
